56e48dab75401c3a1f1128c31567f9136b74e6d7
developertariq <dev.tariqul.islam@gmail.com>
2023-03-07 10:07:48 +0600

    Update decorate.rb

--- decorate.rb
+++ decorate.rb
@@ -10,22 +10,3 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  MAX_NAME_LENGTH = 10
-
-  def correct_name
-    name = @nameable.correct_name
-    if name.length > MAX_NAME_LENGTH
-      name.slice(0, MAX_NAME_LENGTH)
-    else
-      name
-    end
-  end
-end
