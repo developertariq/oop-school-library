cc2b6ee47a3af0ac347f440019eed566466c1f29
developertariq <dev.tariqul.islam@gmail.com>
2023-03-08 14:37:51 +0600

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,13 @@
 class Student < Person
   attr_accessor :classroom
-
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  
+  def initialize( age, name = "Unknown", parent_permission = true, classroom = nil)
+    super(age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self) if classroom
   end
-
+  
   def play_hooky
-    '¯\\_(ツ)_/¯'
+    "¯\\_(ツ)_/¯"
   end
 end
