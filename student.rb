110ddf8d371eef131bc0841d9df1ae5c8ff8801c
developertariq <dev.tariqul.islam@gmail.com>
2023-03-08 14:10:15 +0600

    Implement changes

--- student.rb
+++ student.rb
@@ -1,4 +1,6 @@
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
