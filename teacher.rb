fda242221389f3c73b6efa066e4146fc38d7b7af
developertariq <dev.tariqul.islam@gmail.com>
2023-03-15 23:52:23 +0600

    Add create_object method

--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,6 @@
+require_relative './person'
+require 'json'
+
 class Teacher < Person
   def initialize(age, name = 'Unknown', parent_permission: true, specialization: nil)
     super(age, name, parent_permission: parent_permission)
@@ -6,5 +9,13 @@ class Teacher < Person
 
   def can_use_services?
     true
+  end
+
+  def create_object
+    super()
+    object_data = @object_data
+    object_data[:specialization] = @specialization
+    object_data[:class] = 'Teacher'
+    object_data
   end
 end
