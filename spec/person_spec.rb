9ed26e6e5fe7f4c02eaef1843143d7621a6c6dc0
developertariq <dev.tariqul.islam@gmail.com>
2023-03-16 13:16:14 +0600

    Create person_spec.rb

---
+++ spec/person_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../person'
+
+describe Person do
+  context 'Test for the Person class' do
+    age = 32
+    name = 'Tekhlay'
+    person = Person.new(age, name, parent_permission: false)
+    it 'if it is right class name' do
+      expect(person.class.name) == Person
+    end
+    it 'if it has right age' do
+      expect(person.age).to eq 32
+    end
+    it 'if it has right name' do
+      expect(person.name).to eq 'Tekhlay'
+    end
+  end
+end
